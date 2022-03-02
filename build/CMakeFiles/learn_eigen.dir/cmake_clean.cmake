412ccf16a988aff7d7888b909e96d849fb3cd55d
trebladev <2253714301@qq.com>
2022-03-02 17:32:57 +0800

    add ch3 eigen_learning

---
+++ build/CMakeFiles/learn_eigen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn_eigen.dir/ch3/helloeigen.cpp.o"
+  "learn_eigen"
+  "learn_eigen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learn_eigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch3/helloeigen.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+using namespace std;
+int main()
+{
+	//std::cout<< "helloworld" <<std::endl;
+	Eigen::Matrix3d matrix_33 = Eigen::Matrix3d::Random();
+	cout<< matrix_33 << endl;
+	cout<< matrix_33.sum() <<endl;
+	cout<< matrix_33.trace() <<endl;
+	cout<<matrix_33.inverse()<<endl;
+	cout<< matrix_33*matrix_33.inverse() <<endl;
+
+	Eigen::Matrix<double ,50,50> matrix_NN;
+	matrix_NN = Eigen::MatrixXd::Random(50,50);
+	Eigen::Matrix<double,50,1> v_Nd;
+	v_Nd = Eigen::MatrixXd::Random(50,1);
+	clock_t time_stt = clock();
+
+//	Eigen::Matrix<double,50,1> x = matrix_NN.inverse()*v_Nd;
+//	cout<< "time use in normal inverse is"<<1000*(clock()-time_stt)/(double)CLOCKS_PER_SEC<<"ms"<<endl;
+//	cout<< x <<endl;	
+//	time_stt = clock();
+	Eigen::Matrix<double,50,1> x = matrix_NN.colPivHouseholderQr().solve(v_Nd);
+	cout<< "time use in normal inverse is"<<1000*(clock()-time_stt)/(double)CLOCKS_PER_SEC<<"ms"<<endl;
+//	cout<< x <<endl;
+
+	return 0;
+}
